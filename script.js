6e5fea79bf8d6db1d0d4827bd20e580354faa5e6
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2025-11-01 05:35:32 +0200

    Finish Main Functionality
    Completed the project's main functionality of changing grid cells' colors.
    Further features will be additional/optional or code optimization.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Create dynamic grid using JS with CSS grid (inspired by StackOverflow)
 function createGrid(row, column) {
     const gridContainer= document.querySelector(".container");
-    // Use CSS variables to easily change values as needed
-    gridContainer.style.setProperty('--grid-row', row);
-    gridContainer.style.setProperty('--grid-column', column);
     //Add cells as needed based on the grid size
+    gridContainer.style.setProperty('--num-cols', column);
     let cells;
     for (let i=0; i < (row * column); i++) {
         cells= document.createElement('div');
@@ -12,12 +10,31 @@ function createGrid(row, column) {
     }
 
 };
-createGrid(16, 16);
 
+const gridSize= prompt("Choose size as number (like 30 for example)");
+let rows= gridSize;
+let columns= gridSize;
+if (gridSize > 100) {
+    alert("Large number are not advised. 100x100 is the maximum amount that will be applied")
+    rows= 100;
+    columns= 100
+}
+createGrid(rows, columns);
+
+const min= 1;
+const max= 255;
 // Add eventListeners for grid-items to add hover effect
 const gridItems= document.querySelectorAll('.grid-item');
 gridItems.forEach(gridItem => {
     gridItem.addEventListener('mouseover', () => {
-        gridItem.style.background= 'black'
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        gridItem.style.background= `rgb(${r}, ${g}, ${b})`;
     });
 });
+
+const changeSize= document.querySelector(".size");
+changeSize.addEventListener('click', () => {
+    window.location.reload();
+})
